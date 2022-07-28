289201f87cf7e95dd923284c151a86d3e48d6624
Horeb Gastelum <horeb@live.com.mx>
2022-07-28 10:08:47 -0700

    data generated

--- src/app/components/father/father.component.ts
+++ src/app/components/father/father.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { faker } from "@faker-js/faker";
+import * as _ from "lodash";
+export interface IProduct {
+  name: string,
+  price: number,
+  model: string,
+  id: string,
+}
 
 @Component({
   selector: 'app-father',
@@ -6,10 +14,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./father.component.less']
 })
 export class FatherComponent implements OnInit {
-
-  constructor() { }
+  public products!:IProduct[];
 
   ngOnInit(): void {
+    this.products = this.generateRandomProductGroup();
   }
+
+
+  // ! @ data generators
+  private generateRandomProductGroup(): Array<IProduct> {
+    const productsArray: Array<IProduct> = [];
+    for (let k = 0; k < 20; k++) {
+      productsArray.push(this.generateRandomProduct());
+    }
+    return productsArray;
+  }
+
+  private generateRandomProduct(): IProduct {
+    return {
+      name: faker.vehicle.vehicle(),
+      price: parseFloat(faker.commerce.price()),
+      model: faker.vehicle.model(),
+      id: _.uniqueId(),
+    }
+  }
+
 
 }
